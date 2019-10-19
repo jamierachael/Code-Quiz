e22b34eb8c5f2be0a19e47d7f39b810c441f9376
jamierachael <jamie.rachael@gmail.com>
2019-10-18 21:46:55 -0400

    changed quizz.js to questions.js

--- questions.js
+++ questions.js
@@ -1,3 +1,16 @@
+// Pseudocode 
+// Create a timer attached to a button with a starting value of 0
+// Whnen timer is pressed start a reverse countdown
+// Create a 0 for countdown 
+
+// When countdown starts, start quiz
+// Append the question: choices
+// When user selects the right answer, textcontent "Correct!"
+// When user selects the right answer, textcontent "Wrong!"
+
+// Final score will keep track of how many the user got right 
+// Left over time will be deducted from final score 
+
 // Use this keyword: 
 var questions = [
     {
@@ -10,8 +23,22 @@ var questions = [
         choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
         answer: "parentheses"
     },
-    // Fill in all question into this one object
-    ///etc.
+    {
+        title: "Arrays in Javascript can be used to store ____.",
+        choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+        answer: "all of the above"
+    },
+    {
+        title: "String values must be enclosed within ____ when being assigned to variables.",
+        choices: ["commas", "curly brackets", "quotes", "parenthesis"],
+        answer: "quotes"
+    },
+    {
+        title: "A very useful tool for used during development and debugging for printing content to the debugger is:",
+        choices: ["Javascript", "terminal / bash", "for loops", "console log"],
+        answer: "console log"
+    },
+
 ];
 
 var score = 0;
@@ -22,13 +49,14 @@ var questionIndex = 0;
 var currentTime = document.querySelector("#currentTime");
 var timer = document.querySelector("#startTime");
 var questionsDiv = document.querySelector("#questionsDiv");
-
-
+var wrapper = document.querySelector("#wrapper");
 
 // Seconds left is 15 seconds per question:
-var secondsLeft = 5;
+var secondsLeft = 76;
 // Holds interval time
 var holdInterval = 0;
+// Holds penalty time
+var penalty = 10;
 // var newVariable;
 var ulCreate = document.createElement("ul");
 
@@ -39,11 +67,15 @@ timer.addEventListener("click", function () {
     if (holdInterval === 0) {
         holdInterval = setInterval(function () {
             secondsLeft--;
-            currentTime.textContent = secondsLeft + " seconds left for quiz";
+            currentTime.textContent = "Time: " + secondsLeft;
 
             if (secondsLeft <= 0) {
                 clearInterval(holdInterval);
+                // Works, enble after local storage is set
+                allDone();
                 // alert("Zero!");
+                // Works, enble after local storage is set
+                currentTime.textContent = "Time's up!";
             }
         }, 1000);
     }
@@ -74,43 +106,35 @@ function render(questionIndex) {
 
 function compare(event) {
     var element = event.target;
-    // var userResponse = questions[i].answer;
+
     if (element.matches("li")) {
-        // console.log(liCreate);
+
         var createDiv = document.createElement("div");
         createDiv.setAttribute("id", "createDiv");
 
-        // correct answer works, does not text content to div
-
         if (element.textContent == questions[questionIndex].answer) {
             score++;
-            alert("Correct!");
-            // no longer working
+            // alert("Correct!");
             createDiv.textContent = "Correct! The answer is:  " + questions[questionIndex].answer;
         } else {
-            alert("Wrong!");
-            // Can use the same above for element.textContent;
-            // not working
+            // alert("Wrong!");
+            // Will deduct -5 seconds off secondsLeft for wrong answers
+            secondsLeft = secondsLeft - penalty;
             createDiv.textContent = "Wrong! The correct answer is:  " + questions[questionIndex].answer;
         }
-        alert("You got  " + score + "/" + questions.length);
-        console.log(score);
-        // Should this be changed?
+        // alert("You got  " + score + "/" + questions.length);
+        // console.log(score);
 
     }
     // Question Index is not added 1 when on second question
     questionIndex++;
     // render(questionIndex);
-    console.log(questionIndex);
-    // questionsDiv.appendChild(createDiv);
+    // console.log(questionIndex);
 
-    // Needs to detect end of array: 
-    // lastIndexOf(questionIndex);
-    //  break; ?
     if (questionIndex >= questions.length) {
         // Proper spot to call this: 
         allDone();
-        alert("end of quiz!");
+        createDiv.textContent = "End of quiz!" + " " + "You got  " + score + "/" + questions.length + " Correct!";
     } else {
         render(questionIndex);
     }
@@ -143,14 +167,15 @@ function allDone() {
         var createP2 = document.createElement("p");
         clearInterval(holdInterval);
         // alert("Zero!");
-        createP2.textContent = "You had " + timeRemaining + " seconds left!";
+        createP.textContent = "Your final score is: " + timeRemaining;
+        // createP2.textContent = "You had " + timeRemaining + " seconds left!";
         questionsDiv.appendChild(createP2);
     }
 
     // Label
     var createLabel = document.createElement("label");
     createLabel.setAttribute("id", "createLabel");
-    createLabel.textContent = "Enter your initials"
+    createLabel.textContent = "Enter your initials ";
 
     questionsDiv.appendChild(createLabel);
 
@@ -165,11 +190,14 @@ function allDone() {
     var createSubmit = document.createElement("button");
     createSubmit.setAttribute("type", "submit");
     createSubmit.setAttribute("id", "Submit");
-    // Not working 
     createSubmit.textContent = "Submit";
 
-    // Not working 
     questionsDiv.appendChild(createSubmit);
 
 }
 
+
+
+        // Event listener on button or select 
+        // Add answer as data attribute on question - then compare value of button or selct to see if correct answer
+        // Append each choices, needs an event lister for each 
